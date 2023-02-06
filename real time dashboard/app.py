90ddb55e60c6c453b39a48b6336768753a5e2b89
kristine karimi <64679787+tinajs2018@users.noreply.github.com>
2023-02-06 09:23:01 +0300

    Add files via upload

---
+++ real time dashboard/app.py
@@ -0,0 +1,58 @@
+import numpy as np
+#web application
+import streamlit as st
+import pandas as pd
+#simulate real time data and time loop
+import time
+#interactive charts 
+import  plotly .express as px
+#reading the data 
+df = pd.read_csv('bank-marketing-analysis-master\bank.csv')
+st .set_page_config(
+page_title="Real-time Data Science Dashboard",
+page_icon='zzzzz',
+layout='Wide'
+)
+#dashboard title
+st.title('Real_time/Live data science dashaboard')
+#top_level filter
+job_filter=st.selectbox('Select the job',pd.unique(df['Job']))
+#creating a single  element container
+placeholder =st.empty()
+#dataframe filter
+df=df[df['job']==job_filter]
+#near real_time or live feed simulation
+for seconds in range(200):
+    #while true
+    df['age_new'] = df['age'] * np.random.choice(range(1,5))
+    df['balance_new'] = df['balance'] * np.random.choice(range(1,5))
+    #creating KPIS
+    avg_age = np.mean(df['age_new']) 
+
+    count_married = int(df[(df["marital"]=='married')]['marital'].count() + np.random.choice(range(1,30)))
+    
+    balance = np.mean(df['balance_new'])
+    with placeholder.container():
+        # create three columns
+        kpi1, kpi2, kpi3 = st.columns(3)
+
+        # fill in those three columns with respective metrics or KPIs 
+        kpi1.metric(label="Age ⏳", value=round(avg_age), delta= round(avg_age) - 10)
+        kpi2.metric(label="Married Count 💍", value= int(count_married), delta= - 10 + count_married)
+        kpi3.metric(label="A/C Balance ＄", value= f"$ {round(balance,2)} ", delta= - round(balance/count_married) * 100)
+        fig_col1, fig_col2 = st.columns(2)
+        with fig_col1:
+            st.markdown("### First Chart")
+            fig = px.density_heatmap(data_frame=df, y = 'age_new', x = 'marital')
+            st.write(fig)
+        with fig_col2:
+            st.markdown("### Second Chart")
+            fig2 = px.histogram(data_frame = df, x = 'age_new')
+            st.write(fig2)
+        st.markdown("### Detailed Data View")
+        st.dataframe(df)
+        time.sleep(1)
+    #placeholder.empty()
+
+
+ 
